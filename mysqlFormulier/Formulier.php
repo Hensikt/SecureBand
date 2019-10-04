6ee013aff91da6c4a16a98ada3811c914d5879f5
Bjørn Emmaneel <27728@ma-web.nl>
2019-10-04 11:27:16 +0200

    update

---
+++ mysqlFormulier/Formulier.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hensikt
+ * Date: 2019-10-03
+ * Time: 10:28
+ */
+
+
+?>
+<html lang="nl">
+<head>
+    <title>Formulier</title>
+    <meta name="author" content="Bjørn Emmaneel">
+    <link rel="stylesheet" href="css/master.css">
+</head>
+<body>
+<div class="formulier">
+    <form action="">
+        <label>Voornaam:       <input type="text"></label>
+        <label>Achternaam:     <input type="text"></label>
+        <label>Straat:         <input type="text"></label>
+        <label>Huisnmr:        <input type="number"></label>
+        <label>Postcode:       <input type="number"> <input type="text"></label>
+        <label>Woonplaats:     <input type="text"></label>
+        <label>E-mail:         <input type="text"></label>
+        <label>Telefoonnummer: <input type="number"></label>
+        <input type="submit">
+    </form>
+</div>
+</body>
+</html>
