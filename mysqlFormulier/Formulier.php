730dfb0524d743afbdb5dcca33d5adf5628c3a4b
Hensikt <bjornemmaneel@gmail.com>
2019-10-08 19:35:17 +0200

    update

--- mysqlFormulier/Formulier.php
+++ mysqlFormulier/Formulier.php
@@ -15,18 +15,38 @@
     <link rel="stylesheet" href="css/master.css">
 </head>
 <body>
-<div class="formulier">
-    <form action="">
-        <label>Voornaam:       <input type="text"></label>
-        <label>Achternaam:     <input type="text"></label>
-        <label>Straat:         <input type="text"></label>
-        <label>Huisnmr:        <input type="number"></label>
-        <label>Postcode:       <input type="number"> <input type="text"></label>
-        <label>Woonplaats:     <input type="text"></label>
-        <label>E-mail:         <input type="text"></label>
-        <label>Telefoonnummer: <input type="number"></label>
-        <input type="submit">
-    </form>
+<div class="wrapper">
+    <div class="contactForm">
+        <div class="contactForm__inputFields">
+            <input type="text" class="input" placeholder="Voornaam">
+            <input type="text" class="input" placeholder="Achternaam">
+            <input type="text" class="input" placeholder="Straat">
+            <input type="text" class="input" placeholder="Huisnummer">
+            <span><input type="text" class="input next" placeholder="Postcode"> <input type="text" class="input next" placeholder="nummer"></span>
+            <input type="text" class="input" placeholder="Woonplaats">
+
+        </div>
+        <div class="contactForm__extraInputFields">
+            <input type="text" class="input" placeholder="Email Address">
+            <input type="text" class="input" placeholder="Telefoonnummer">
+            <input type="submit" class="btn" placeholder="Send">
+        </div>
+
+    </div>
 </div>
 </body>
 </html>
+
+<!--<div class="formulier">-->
+<!--    <form action="">-->
+<!--        <label>Voornaam:       <input type="text"></label>-->
+<!--        <label>Achternaam:     <input type="text"></label>-->
+<!--        <label>Straat:         <input type="text"></label>-->
+<!--        <label>Huisnmr:        <input type="number"></label>-->
+<!--        <label>Postcode:       <input type="number"> <input type="text"></label>-->
+<!--        <label>Woonplaats:     <input type="text"></label>-->
+<!--        <label>E-mail:         <input type="text"></label>-->
+<!--        <label>Telefoonnummer: <input type="number"></label>-->
+<!--        <input type="submit">-->
+<!--    </form>-->
+<!--</div>-->
